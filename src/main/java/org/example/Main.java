a4185e0745e44e343835774fb77c903bc6b711ba
akshidhaa114 <akshidhaa14@gmail.com>
2026-04-15 14:56:35 +0530

    Coded UC7-Display Hello World in console

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,14 @@
-public class UC4Hello {
+public class UC7 {
 
     public static void main(String[] args) {
+
         if (args.length > 0) {
-            // Print "Hello" + all command-line arguments
-            System.out.print("Hello");
-            for (String arg : args) {
-                System.out.print(" " + arg);
-            }
-            System.out.println();
+            // Join all command-line arguments with a space
+            String joined = String.join(" ", args);  // uses String.join()[web:16]
+            System.out.println("Hello " + joined);
         } else {
-            // Default message when no arguments are passed
-            System.out.println("Hello, World!");
+            // Default message when there are no arguments
+            System.out.println("Hello World");
         }
     }
 }
